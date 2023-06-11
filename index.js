9e4ef41cae3572dcc8c5f4c5b7199c1749923a0a
Lydia Nyamoita <lydianyamo18@outlook.com>
2023-06-11 17:22:11 +0300

    completed assigment

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(hello) {
+ return "hello".toUpperCase();
+}
+
+console.log(shout());
+
+function whisper(HELLO) {
+  return "HELLO".toLowerCase()
+}
+
+function logShout(Hello) {
+  console.log("Hello".toUpperCase())
+}
+
+function  logWhisper(HELLO){
+  console.log("HELLO".toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(namePhrase){
+  console.log(`${namePhrase}`);
+}
+
+sayHiToHeadphonedRoommate("I can't hear you");
+sayHiToHeadphonedRoommate("YES INDEED!");
+sayHiToHeadphonedRoommate("I would love to");
+
